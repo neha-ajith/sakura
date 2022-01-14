582dca33813a3e3f28589e0916b8ee0c4a3c5281
“neha-ajith” <nehaajith27@gmail.com>
2022-01-14 21:05:51 +0530

    desktop version complete

--- lib/views/post_view.dart
+++ lib/views/post_view.dart
@@ -14,33 +14,38 @@ class _PostViewState extends State<PostView> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: IntrinsicHeight(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              VerticalDivider(
-                color: Color.fromARGB(255, 73, 69, 69),
-                thickness: 0.5,
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            VerticalDivider(
+              color: Color.fromARGB(255, 73, 69, 69),
+              thickness: 0.5,
+              endIndent: 50,
+            ),
+            SizedBox(
+              width: 1000,
+              child: Column(
+                children: [
+                  Text(
+                    "Heading 1",
+                    style: TextStyle(fontSize: 50, fontFamily: "Heading"),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat nisl pretium fusce id velit ut. Tempus iaculis urna id volutpat lacus laoreet. Nec ultrices dui sapien eget mi proin sed. Varius duis at consectetur lorem donec massa sapien faucibus et. Nunc mi ipsum faucibus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat nisl pretium fusce id velit ut. Tempus iaculis urna id volutpat lacus laoreet. Nec ultrices dui sapien eget mi proin sed. Varius duis at consectetur lorem donec massa sapien faucibus et. Nunc mi ipsum faucibus .Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat nisl pretium fusce id velit ut. Tempus iaculis urna id volutpat lacus laoreet. Nec ultrices dui sapien eget mi proin sed. Varius duis at consectetur lorem donec massa sapien faucibus et. Nunc mi ipsum faucibus .Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                    style: TextStyle(fontSize: 20),
+                  )
+                  // PostSample(),
+                ],
               ),
-              SizedBox(
-                width: 1000,
-                child: Column(
-                  children: [
-                    // PostSample(),
-                    // SizedBox(height: 15),
-                    // PostSample(),
-                  ],
-                ),
-              ),
-              VerticalDivider(
-                color: Color.fromARGB(255, 73, 69, 69),
-                thickness: 0.5,
-              ),
-              SideNote()
-            ],
-          ),
+            ),
+            VerticalDivider(
+              color: Color.fromARGB(255, 73, 69, 69),
+              thickness: 0.5,
+              endIndent: 50,
+            ),
+            SideNote()
+          ],
         ),
       ),
     );
